e0994fffbf7f2dfee23493d916cc6559ac7ce388
josueisonfire <josueisonfire@gmail.com>
2017-11-15 04:01:51 +0900

    added help & controls states for the menu

--- public/benchmark2/js/game.js
+++ public/benchmark2/js/game.js
@@ -4,6 +4,7 @@ game.state.add('boot', bootState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
 game.state.add('help', helpState);
+game.start.add('control', controlState);
 
 //change to boot in final version maybe?
 game.state.start('menu');
