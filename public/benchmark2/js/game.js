1fd437b5b18a956ed1376b95410cac0edf3255e2
josueisonfire <josueisonfire@gmail.com>
2017-11-15 03:55:59 +0900

    tester 2

--- public/benchmark2/js/game.js
+++ public/benchmark2/js/game.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 game.state.add('boot', bootState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
+game.state.add('help', helpState);
 
 //change to boot in final version maybe?
 game.state.start('menu');
---
+++ public/benchmark2/js/help.js
@@ -0,0 +1,4 @@
+var helpState = 
+{
+
+};
