68a89613490aa996b1a72d38f9a2f8f95b21daa4
SpaceDoog <hysdw3@gmail.com>
2017-11-15 10:50:03 +0900

    more work on play.js - implemented acceleration based movement, world bound setting and camera needs more work

--- public/benchmark2/js/play.js
+++ public/benchmark2/js/play.js
@@ -10,29 +10,46 @@ var playState = {
         
         //create one background for now
         this.background = this.add.sprite(0, 0, 'background');
+        this.background2 = this.add.sprite(0, -600, 'background');
         game.physics.arcade.enable(this.background);
+        game.physics.arcade.enable(this.background2);
         
         //create player character
         this.createPlayer();
         
         //cursor controls
         this.cursors = this.input.keyboard.createCursorKeys();
+        
+        //camera tracking var
+        this.cameraYMin = 99999;
     },
     
     update: function(){
         this.playerMovement();
-        game.debug.text(this.player.body.velocity.y, 32, 32);
+        //debug stuff TODO:: erase
+        game.debug.text("YChange: " + this.player.yChange, 32, 32);
+        game.debug.text("Y:       " + this.player.y, 32, 64);
+        game.debug.text("cameraYMIN: "+this.cameraYMin, 32, 96);
+        game.debug.text("yOrig:   " + this.player.yOrig, 32, 120);
+        game.debug.text("this.game.height: "+this.game.height, 32, 140);
+        
+        //The height of the world is adjusted to match the furthest the player has reached.
+        this.world.setBounds(0, -this.player.yChange, this.world.width, this.game.height + this.player.yChange);
+        
+        //custom camera follow; only moves up
+        this.cameraYMin = Math.min(this.cameraYMin, this.player.y - this.game.height + 130);
+        this.camera.y = this.cameraYMin;
     },
     
     createPlayer: function(){
         this.player = game.add.sprite(this.world.centerX, this.world.centerY, 'player');   this.player.anchor.set(0.5);
         this.player.maxSpeed = 300;
         
-        //track where the player started and track change in y distance
-        this.player.yOrig = this.player.position.y;
+        //variables to track where the player started and track change in y distance
+        this.player.yOrig = this.player.y;
         this.player.yChange = 0;
         
-        //physics
+        //player physics
         game.physics.arcade.enable(this.player);
         this.player.body.collideWorldBounds = true;
     },
@@ -54,7 +71,7 @@ var playState = {
                 this.player.body.velocity.y = 0;
         }
         if(this.cursors.left.isDown && this.player.body.velocity.x > -this.player.maxSpeed){
-            this.player.body.velocity.x -= inc;  
+            this.player.body.velocity.x -= inc;
         }
         else if(this.cursors.right.isDown && this.player.body.velocity.x < this.player.maxSpeed){
             this.player.body.velocity.x += inc;
@@ -66,9 +83,10 @@ var playState = {
             if (this.player.body.velocity.x < inc && this.player.body.velocity.x > -inc)
                 this.player.body.velocity.x = 0;
         }
+        //TODO:: slow down diagonal movements --by a factor of 1/sqrt(2)?--
     
-    //track the maximum distance player has travelled
-    this.player.yChange = Math.max(this.player.yChange, Math.abs(this.player.position.y - this.player.yOrig));
+    //track the maximum distance player has traveled
+    this.player.yChange = Math.max(this.player.yChange, Math.abs(this.player.y - this.player.yOrig));
   }
 };
 
