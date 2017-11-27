fe42f7e7cacdf27346b0b1b366bc77624986e3e6
SpaceDoog <hysdw3@gmail.com>
2017-11-27 23:09:52 +0900

    play.js many functions WIP... goin to slp added txt file of things we need

--- public/benchmark2/js/play.js
+++ public/benchmark2/js/play.js
@@ -3,21 +3,30 @@ var playState = {
         //load all assets
         this.load.image('background', 'assets/background.png');
         this.load.spritesheet('player', 'assets/dude.png', 32, 48);
+        this.load.image('car1', 'assets/car1.png');
     },
     
     create: function(){
         this.backgroundCounter = 0;
-        //TODO:: createCars, createBackgrounds...
         
         //create background images to repeat
         this.createBackground();
         
+        //create cars        
+        var car1 = this.add.sprite(408,400, 'car1');
+        car1.scale.setTo(0.25,0.25);
+        game.physics.arcade.enable(car1);
+        car1.body.velocity.y = -450;
+        
         //create player character
         this.createPlayer();
-        
+                
         //cursor controls
         this.cursors = this.input.keyboard.createCursorKeys();
-        
+        this.runKey = this.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+        this.jumpKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        this.crouchKey = this.input.keyboard.assKey(Phaser.Keyboard.E);        
+                
     },
     
     update: function(){
@@ -35,12 +44,21 @@ var playState = {
         //The bounds of the world is adjusted to match the furthest the player has reached. i.e. the world moves with the player albeit only upwards
         this.world.setBounds(0, -this.player.yChange, this.world.width, this.game.height);
         
+        //player sprint
+        if (this.runKey.isDown){
+            this.player.maxSpeed = 750;
+        }
+        else{
+            this.player.maxSpeed = 300;
+        }
     },
     
 //create-related functions
     createPlayer: function(){
         this.player = game.add.sprite(this.world.centerX, this.world.height -300, 'player');   this.player.anchor.set(0.5);
         this.player.maxSpeed = 300;
+        this.player.isDead = false;
+        this.player.jumped = false;
         
         //variables to track where the player started and track change in y distance
         this.player.yOrig = this.player.y;
@@ -53,7 +71,6 @@ var playState = {
     
     createBackground: function(){
         this.backgrounds = this.add.group();
-        this.backgrounds.enableBody = true;
         this.backgrounds.createMultiple(3, 'background');
         for (var i=0; i<3; i++){
             this.createBackgroundOne(-600*i);
@@ -66,6 +83,11 @@ var playState = {
         background.reset(0, y);
         this.backgroundCounter++;
         return background;
+    },
+    
+    createCars: function(){
+        this.cars = this.add.group();
+        //TODO:: implement
     },
     
 //update-related functions    
@@ -108,9 +130,19 @@ var playState = {
                 this.player.body.velocity.x = 0;
         }
         //TODO:: slow down diagonal movements --by a factor of 1/sqrt(2)?--
-    
+        //if(2keyspressed for any 4 directions) player velocity / sqrt(2)
     //track the maximum distance player has traveled
     this.player.yChange = Math.max(this.player.yChange, -(this.player.y - this.player.yOrig));
+    },
+    
+    playerJump: function(){
+        if (this.jumpKey.isDown && !this.player.jumped){
+            
+        }
+    },
+    
+    playerDead: function(){
+        
     },
 };
 
